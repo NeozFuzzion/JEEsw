0f8c8c7c43b462b80970a2894b1e56ca80f22f50
NeozFuzzion <lantaaxel@gmail.com>
2022-11-24 08:26:59 +0100

    Ajout dépendence lecture json

--- sw/src/main/java/projectJEE/sw/SwApplication.java
+++ sw/src/main/java/projectJEE/sw/SwApplication.java
@@ -1,5 +1,8 @@
 package projectJEE.sw;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.core.io.ClassPathResource;
 import projectJEE.sw.dbRepository.MonsterRepository;
 import projectJEE.sw.dbRepository.RuneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.io.File;
+import java.io.FileReader;
 
 @SpringBootApplication
 public class SwApplication {
@@ -27,7 +33,12 @@ public class SwApplication {
 
 			System.out.println(runeRepository.findAll());
 			System.out.println(monsterRepository.findAll());
+			File file = new ClassPathResource("data/NeozFuzzion-840111.json").getFile();
 
+			JSONParser jsonP = new JSONParser();
+
+				JSONObject jsonO = (JSONObject)jsonP.parse(new FileReader(file));
+				System.out.println(jsonO.get("unit_list"));
 
 
 		};
