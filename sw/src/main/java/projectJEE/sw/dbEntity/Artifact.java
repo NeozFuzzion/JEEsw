d87caef746236db52934683fc638c66856e1e634
Izilyph <edout1302@gmail.com>
2022-11-25 08:22:57 +0100

    Ajout ArtefactCorrigé

--- sw/src/main/java/projectJEE/sw/dbEntity/Artifact.java
+++ sw/src/main/java/projectJEE/sw/dbEntity/Artifact.java
@@ -1,8 +1,10 @@
 package projectJEE.sw.dbEntity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
 public class Artifact {
 
     @Id @Column
--- sw/src/main/java/projectJEE/sw/dbRepository/ArtifactRepository.java
+++ sw/src/main/java/projectJEE/sw/dbRepository/ArtifactRepository.java
@@ -1,8 +1,11 @@
 package projectJEE.sw.dbRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
 import projectJEE.sw.dbEntity.Artifact;
 
+@Repository
 public interface ArtifactRepository extends JpaRepository<Artifact, Long> {
     public Artifact findFirstByIdArtifact(long id);
 }
