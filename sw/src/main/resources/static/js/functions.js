4932d91378ad7b38280b3fd1538028871e83f84e
DUMODA <dumoda.64@gmail.com>
2022-12-20 20:50:36 +0100

    css monsters

--- sw/src/main/resources/static/js/functions.js
+++ sw/src/main/resources/static/js/functions.js
@@ -11,3 +11,37 @@ function hideRightSide() {
 function showKEK() {
     document.getElementsByClassName('kek')[0].style.display = "flex";
 }
+
+function showSkillDescription(skill_id) {
+    let skill_selected = document.getElementsByClassName("skill-selected")[0].id;
+    document.getElementById(skill_selected).className = "skill-not-selected";
+    document.getElementById(skill_id).className = "skill-selected";
+
+    document.getElementById(skill_selected + "-description").style.display = "none";
+    document.getElementById(skill_id + "-description").style.display = "flex";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let nb_stars = parseInt(document.getElementById("monster-stars").innerHTML);
+    let awakening_lvl = parseInt(document.getElementById("awakening_level").innerHTML);
+    let error = false;
+
+    if (awakening_lvl == 1) {
+        nb_stars += 1;
+    }
+    if (awakening_lvl == 2) {
+        nb_stars = 6;
+    }
+
+    for (let i = 0; i < nb_stars; i++) {
+        if (awakening_lvl == 0) {
+            document.getElementsByClassName("monster-stars-images")[0].innerHTML += "<img src='/images/stars/star-unawakened.png'>";
+        } else if (awakening_lvl == 1) {
+            document.getElementsByClassName("monster-stars-images")[0].innerHTML += "<img src='/images/stars/star-awakened.png'>";
+        } else if (awakening_lvl == 2) {
+            document.getElementsByClassName("monster-stars-images")[0].innerHTML += "<img src='/images/stars/star-awakened2.png'>";
+        } else {
+            error = true;
+        }
+    }
+});
